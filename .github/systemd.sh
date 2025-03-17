b612aab2056dad2d6a404c5d88a40095c79dc9c7
Samuel Jimenez <53528911+samuel-jimenez@users.noreply.github.com>
2025-03-17 15:50:20 -0500

    service files

--- .github/systemd.sh
+++ .github/systemd.sh
@@ -1,51 +1,5 @@
 #!/bin/bash
 
-echo '
-[Unit]
-Description=sccache-dist buildserver
-Wants=network-online.target
-After=network-online.target
-
-[Service]
-Environment="SCCACHE_SYSLOG=info"
-Environment="SCCACHE_NO_DAEMON=1"
-Environment="RUST_BACKTRACE=1"
-EnvironmentFile=/etc/conf.d/sccache/sccache.conf
-ExecStart=/usr/bin/sccache-dist server --config /etc/conf.d/sccache/buildserver-config.toml --syslog $SCCACHE_SYSLOG
-
-[Install]
-WantedBy=multi-user.target' | sudo tee /etc/systemd/system/sccache-buildserver-overlay.service
-
-echo '
-[Unit]
-Description=sccache-dist buildserver
-Wants=network-online.target
-After=network-online.target
-
-[Service]
-Environment="SCCACHE_SYSLOG=info"
-Environment="SCCACHE_NO_DAEMON=1"
-Environment="RUST_BACKTRACE=1"
-EnvironmentFile=/etc/conf.d/sccache/sccache.conf
-ExecStart=/usr/bin/sccache-dist server --config /etc/conf.d/sccache/buildserver-docker-config.toml --syslog $SCCACHE_SYSLOG
-
-[Install]
-WantedBy=multi-user.target' | sudo tee /etc/systemd/system/sccache-buildserver-docker.service
-
-echo '
-[Unit]
-Description=sccache-dist scheduler
-Wants=network-online.target
-After=network-online.target
-
-[Service]
-Environment="SCCACHE_SYSLOG=info"
-Environment="SCCACHE_NO_DAEMON=1"
-Environment="RUST_BACKTRACE=1"
-EnvironmentFile=/etc/conf.d/sccache/sccache.conf
-ExecStart=/usr/bin/sccache-dist scheduler --config /etc/conf.d/sccache/scheduler-config.toml --syslog $SCCACHE_SYSLOG
-
-[Install]
-WantedBy=multi-user.target' | sudo tee /etc/systemd/system/sccache-scheduler.service
-
+ls -la
+sudo mv ./.github/system/* /etc/systemd/system
 sudo systemctl daemon-reload
