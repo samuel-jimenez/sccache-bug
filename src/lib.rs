80b1a45da6758e669b74eb5e14298209bac14f6a
Samuel Jimenez <53528911+samuel-jimenez@users.noreply.github.com>
2025-03-18 15:46:43 -0500

    smol

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,4 @@
-use std::fmt::Debug;
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-#[repr(transparent)]
 pub struct NodeId(u64);
-
-impl From<u64> for NodeId {
-    fn from(id: u64) -> Self {
-        NodeId(id)
-    }
-}
-
-impl NodeId {
-    pub const EMPTY: Self = NodeId(0);
-
-    pub fn is_empty(&self) -> bool {
-        self == &Self::EMPTY
-    }
-}
 
 impl<'a> test_lib::Key for NodeId {
     fn compare(data1: &[u8], data2: &[u8]) -> std::cmp::Ordering {
